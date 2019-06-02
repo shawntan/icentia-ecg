41e748a252d45baf043ae22c94423856225eb65e
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-06-02 16:22:27 -0400

    scale

--- prepare_embeddings.py
+++ prepare_embeddings.py
@@ -11,8 +11,9 @@ frame_length = 2**11+1
 filename = ""
 labels = None
 results = []
+
 for index, row in test_labels.iterrows():
-    new_filename = "/home/cohenjos/projects/rpp-bengioy/jpcohen/icentia12k/" + str(row["sample"]) + "_batched.pkl.gz"
+    new_filename = sys.argv[1] + "/" + str(row["sample"]) + "_batched.pkl.gz"
     if filename != new_filename: # do fancy caching
         print(new_filename)
         filename = new_filename
@@ -25,13 +26,14 @@ for index, row in test_labels.iterrows():
 
     # compute the embedding
     
-    emb = input_seq # baseline
+    emb = np.copy(input_seq) # baseline
     
     results.append([row["sample"],row["segment"],row["frame"],*emb])
     
-    
+print("Creating dataframe")
 results_df = pd.DataFrame(results, columns=["sample", "segment", "frame", *[""]*(len(results[0])-3)])
 results_df.index.name = "id"
 
-results_df.to_csv("test_emb.csv")
+print("Writing CSV")
+results_df.to_csv("test_emb.csv.gz")
 
