029ced1bd1e4b628bc0803c1041ace459b58d256
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-06-21 11:46:17 -0400

    add script

--- process.sh
+++ process.sh
@@ -2,4 +2,4 @@
 
 
 
-find ~/projects/rpp-bengioy/jpcohen/icentia-mila-research -name "*npz" ! -name "*batched*" -exec echo {} \; -exec python data_split.py {} \;
+find ~/projects/rpp-bengioy/jpcohen/icentia-mila-research -name "*npz" ! -name "*batched*" -exec echo {} \; -exec python data_split.py {} ~/projects/rpp-bengioy/jpcohen/icentia12k/ \;
