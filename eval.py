87860b3ca0db7ba83c2cc3812b124c4a826e8283
Joseph Paul Cohen <joseph@josephpcohen.com>
2019-06-23 01:39:36 -0400

    faster eval

--- eval.py
+++ eval.py
@@ -4,13 +4,14 @@ import matplotlib.pylab as plt
 import sys
 import pandas as pd
 import sklearn, sklearn.model_selection, sklearn.neighbors
+import gzip
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('embeddings_file', help='File with embeddings')
 parser.add_argument('num_train_examples', nargs='?', type=int, default=1000, help='')
 parser.add_argument('num_trials', nargs='?', type=int, default=10, help='')
-parser.add_argument('labels_file', nargs='?', default="test_labels.csv.zip", help='')
+parser.add_argument('labels_file', nargs='?', default="test_labels.csv.gz", help='')
 args = parser.parse_args()
 
 print(args)
@@ -22,29 +23,64 @@ def create_index(df):
     del df["segment"]
     del df["frame"]
     df.set_index("id", inplace=True)
-    
 
-data = pd.read_csv(args.embeddings_file)
-labels = pd.read_csv(args.labels_file)
 
-create_index(data)
-create_index(labels)
+## get counts
+lines_emb = 0
+with gzip.open(args.embeddings_file, 'rb') as f:
+    for line in f:
+        lines_emb += 1
 
-# order by labels
-if len(labels.index) != len(data.index):
+lines_labels = 0
+with gzip.open(args.labels_file, 'rb') as f:
+    for line in f:
+        lines_labels += 1
+
+print("lines_emb:", lines_emb)
+
+if lines_labels != lines_emb:
     print(" !! Issue with coverage of labels. The data must align to the labels.")
     sys.exit()
+    
+        
+np.random.seed(0)
+def getSubset(num_samples):
+    tosample = np.random.choice(lines_emb, num_samples, replace=False)
+    subset = []
+    with gzip.open(args.embeddings_file, 'rb') as f:
+        header = f.readline().decode('ascii')
+        
+        for i, line in enumerate(f):
+            if (i in tosample):
+                subset.append(line.decode('ascii').split(","))
+    data = pd.DataFrame(subset, columns=header.replace(" ","").split(","))
+    create_index(data)
+    
+    subset = []
+    with gzip.open(args.labels_file, 'rb') as f:
+        header = f.readline().decode('ascii')
+        
+        for i, line in enumerate(f):
+            if (i in tosample):
+                subset.append(line.decode('ascii').split(","))
+    labels = pd.DataFrame(subset, columns=header.replace(" ","").split(","))
+    create_index(labels)
+    
+    # order by labels
+    data = data.loc[labels.index]
+    
+    return data, labels
 
-data = data.loc[labels.index]
-
-print("Loaded data", data.shape)
-
-def evaluate(embeddings, labels, num_train_examples, num_test_examples, num_trials):
+def evaluate(num_train_examples, num_test_examples, num_trials):
     
     all_acc = []
     for i in range(num_trials):
+        
+        print("Generating subset", i)
+        data, labels = getSubset(num_train_examples+num_test_examples)
+        
         X, X_test, y, y_test = \
-            sklearn.model_selection.train_test_split(embeddings, labels, 
+            sklearn.model_selection.train_test_split(data, labels, 
                                                      train_size=num_train_examples, 
                                                      test_size=num_test_examples, 
                                                      stratify=labels,
@@ -60,7 +96,9 @@ def evaluate(embeddings, labels, num_train_examples, num_test_examples, num_tria
 
     return np.asarray(all_acc).mean(), np.asarray(all_acc).std()
     
-mean,stdev = evaluate(data, labels, args.num_train_examples, min(3000,len(data)-args.num_train_examples), args.num_trials)
+    
+    
+mean,stdev = evaluate(args.num_train_examples, 3000, args.num_trials)
 
 print("Accuracy:",round(mean,3), "+-", round(stdev,3), "num_trials:",args.num_trials) 
 
